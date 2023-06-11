ddc70ef3c7eedd59622e9e182bdbc6cefa6421f5
adegate <adegati365@gmail.com>
2023-06-12 01:17:01 +0800

    Membuat constructor dan set penjumlahan

--- src/hitung/matematika.java
+++ src/hitung/matematika.java
@@ -1,5 +1,14 @@
 package hitung;
 
 public class matematika {
+    private double bil1,bil2;
     
+    public matematika(double bil1, double bil2){
+        this.bil1 = bil1;
+        this.bil2 = bil2;
+    }
+    
+    double setPenjumlahan(){
+        return bil1+bil2;
+    }
 }
