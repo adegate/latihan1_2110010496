ee6d728e614f018096182f3b1acf529dc36637be
adegate <adegati365@gmail.com>
2023-06-12 01:28:42 +0800

    membuat class matematika beraksi

--- src/hitung/matematikaBeraksi.java
+++ src/hitung/matematikaBeraksi.java
@@ -2,5 +2,12 @@
 package hitung;
 
 public class matematikaBeraksi {
-    
+    public static void main(String[] args) {
+        matematika ade = new matematika(9,6);
+        
+        System.out.println("Hasil Penjumlahan = " + ade.setPenjumlahan());
+        System.out.println("Hasil Pengurangan = " + ade.setPengurangan());
+        System.out.println("Hasil Perkalian = " + ade.setPerkalian());
+        System.out.println("Hasil Pembagian = " + ade.setPembagian());
+    }
 }
