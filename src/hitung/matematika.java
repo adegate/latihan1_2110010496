19b35521eccd291cc3d1bf950bb4a99a167c1343
adegate <adegati365@gmail.com>
2023-06-12 01:23:08 +0800

    Melengkapi Method matematika

--- src/hitung/matematika.java
+++ src/hitung/matematika.java
@@ -8,7 +8,19 @@ public class matematika {
         this.bil2 = bil2;
     }
     
-    double setPenjumlahan(){
+    public double setPenjumlahan(){
         return bil1+bil2;
+    }
+    
+    public double setPengurangan(){
+        return bil1-bil2;
+    }
+     
+    public double setPerkalian(){
+        return bil1*bil2;
+    }
+      
+    public double setPembagian(){
+        return bil1/bil2;
     }
 }
